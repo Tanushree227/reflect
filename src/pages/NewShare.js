61f959c88c7d42eba54c45ed70e4dc86044c2856
Tanushree227 <tanushreedas813@gmail.com>
2024-01-13 15:06:24 +0530

    sending a post http request

--- src/pages/NewShare.js
+++ src/pages/NewShare.js
@@ -4,7 +4,17 @@ import NewShareForm from "../components/share/NewShareForm";
 function NewShare()
 {
     function addShareHandler(shareData)
-    {}
+    {
+        fetch('https://reflect-f0a06-default-rtdb.firebaseio.com/shares.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(shareData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        );
+    }
 
     return(
         <section>

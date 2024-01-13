274772735afe328f50a71df2e3b095d7a0ed74c8
Tanushree227 <tanushreedas813@gmail.com>
2024-01-13 10:07:22 +0530

    added pages of the project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NewShare from "./pages/NewShare";
+import Favorites from "./pages/Favorites";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <div>
-      Reflect
-    </div>
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/newshare" element={<NewShare />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/NewShare.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function NewShare()
+{
+    return(
+        <h1>New Share Page</h1>
+    );
+}
+
+export default NewShare;

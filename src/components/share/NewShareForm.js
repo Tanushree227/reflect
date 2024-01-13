319045a268d83deaacbc55ba76cb68061f618316
Tanushree227 <tanushreedas813@gmail.com>
2024-01-13 14:39:18 +0530

    getting user data and handling form submission

--- src/components/share/NewShareForm.js
+++ src/components/share/NewShareForm.js
@@ -1,25 +1,50 @@
+import { useRef } from 'react';
 import Card from '../ui/Card';
 import classes from './NewShareForm.module.css';
 
 function NewShareForm() {
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const nameInputRef = useRef();
+    const descriptionInputRef = useRef();
+
+
+    function submitHandler(event) 
+    {
+        event.preventDefault();
+
+        const enteredTitle = titleInputRef.current.value;
+        const enteredImage = imageInputRef.current.value;
+        const enteredName = nameInputRef.current.value;
+        const enteredDescription = descriptionInputRef.current.value;
+
+        const shareData = {
+            title: enteredTitle,
+            image: enteredImage,
+            name: enteredName,
+            description: enteredDescription
+        }
+
+    }
+
     return(
         <Card>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={submitHandler}>
                 <div className={classes.control}>
                     <label htmlFor='title'>Title</label>
-                    <input type="text" placeholder='Enter your experience title' required id="title" />
+                    <input type="text" placeholder='Enter your experience title' ref={titleInputRef} required id="title" />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='image'>Image</label>
-                    <input type="url" placeholder='Enter your Experience Image' required id="image" />
+                    <input type="url" placeholder='Enter your Experience Image' ref={imageInputRef} required id="image" />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='name'>Name</label>
-                    <input type="text" placeholder='Enter your Name' required id="name" />
+                    <input type="text" placeholder='Enter your Name' ref={nameInputRef} required id="name" />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='description'>Description</label>
-                    <textarea id='description' placeholder='Tell us in detail about your Experience' required rows='5' ></textarea>
+                    <textarea id='description' placeholder='Tell us in detail about your Experience' ref={descriptionInputRef} required rows='5' ></textarea>
                 </div>
                 <div className={classes.actions}>
                     <button>Add your Experience</button>

117e0ee624368a713bd022a6482f79652413f536
Tanushree227 <tanushreedas813@gmail.com>
2024-01-15 10:57:26 +0530

    implemented count of favorites to show on website

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import classes from './MainNavigation.module.css';
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation()
 {
+    const favoritesCtx = useContext(FavoritesContext);
+
     return(
         <header className={classes.header}>
             <div className={classes.logo}>Reflect</div>
@@ -11,7 +14,7 @@ function MainNavigation()
                 <ul>
                     <li><Link to="/" exact>Home</Link></li>
                     <li><Link to="/newshare">Share Experience</Link></li>
-                    <li><Link to="/favorites">Favorites</Link></li>
+                    <li><Link to="/favorites">Favorites<span className={classes.badge}>{favoritesCtx.totalFavorites}</span></Link></li>
                     <li className={classes.login}><Link to="/login">Log In</Link></li>
                 </ul>
             </nav>

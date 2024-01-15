b77f7889a3e2caa52d2952808c95017e7b3e1296
Tanushree227 <tanushreedas813@gmail.com>
2024-01-15 15:52:36 +0530

    formatted the document

--- src/components/share/SignupForm.js
+++ src/components/share/SignupForm.js
@@ -2,38 +2,36 @@ import React from "react";
 import { Form, Input } from "antd";
 import { Link } from "react-router-dom";
 import "./signupform.css";
-import { useRef } from 'react';
+import { useRef } from "react";
 
-function SignupForm(props)
-{
+function SignupForm(props) {
   const fullnameInputRef = useRef();
-    const emailInputRef = useRef();
-    const phoneInputRef = useRef();
-    const passwordInputRef = useRef();
-    const c_passwordInputRef = useRef();
+  const emailInputRef = useRef();
+  const phoneInputRef = useRef();
+  const passwordInputRef = useRef();
+  const c_passwordInputRef = useRef();
 
-    function submitHandler(event) 
-    {
-        event.preventDefault();
+  function submitHandler(event) {
+    event.preventDefault();
 
-        const enteredName = fullnameInputRef.current.value;
-        const enteredEmail = emailInputRef.current.value;
-        const enteredPhone = phoneInputRef.current.value;
-        const enteredPassword = passwordInputRef.current.value;
-        const enteredCPassword = c_passwordInputRef.current.value;
+    const enteredName = fullnameInputRef.current.value;
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPhone = phoneInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    const enteredCPassword = c_passwordInputRef.current.value;
 
-        const userData = {
-            fullname: enteredName,
-            email: enteredEmail,
-            phone: enteredPhone,
-            password: enteredPassword,
-            c_password: enteredCPassword
-        };
-        props.onSignup(userData);
+    const userData = {
+      fullname: enteredName,
+      email: enteredEmail,
+      phone: enteredPhone,
+      password: enteredPassword,
+      c_password: enteredCPassword,
     };
-      
-    return (
-        <div className="form-container">
+    props.onSignup(userData);
+  }
+
+  return (
+    <div className="form-container">
       <Form
         layout="vertical"
         onSubmit={submitHandler}
@@ -41,19 +39,44 @@ function SignupForm(props)
       >
         <h1 className="text-center">Create your Account</h1>
         <Form.Item label="Name" name="fullname">
-          <Input type="text" placeholder="Enter your Full Name" ref={fullnameInputRef} required />
+          <Input
+            type="text"
+            placeholder="Enter your Full Name"
+            ref={fullnameInputRef}
+            required
+          />
         </Form.Item>
         <Form.Item label="Email" name="email">
-          <Input type="email" placeholder="Enter your Email Id" ref={emailInputRef} required />
+          <Input
+            type="email"
+            placeholder="Enter your Email Id"
+            ref={emailInputRef}
+            required
+          />
         </Form.Item>
         <Form.Item label="Phone" name="phone">
-          <Input type="phone" placeholder="Enter your Phone Number" ref={phoneInputRef} required />
+          <Input
+            type="phone"
+            placeholder="Enter your Phone Number"
+            ref={phoneInputRef}
+            required
+          />
         </Form.Item>
         <Form.Item label="Password" name="password">
-          <Input type="password" placeholder="Enter your Password" ref={passwordInputRef} required />
+          <Input
+            type="password"
+            placeholder="Enter your Password"
+            ref={passwordInputRef}
+            required
+          />
         </Form.Item>
         <Form.Item label="Confirm Password" name="c_password">
-          <Input type="password" placeholder="Re-Type your Password" ref={c_passwordInputRef} required />
+          <Input
+            type="password"
+            placeholder="Re-Type your Password"
+            ref={c_passwordInputRef}
+            required
+          />
         </Form.Item>
         <p>
           Already have an Account?
@@ -66,7 +89,7 @@ function SignupForm(props)
         </div>
       </Form>
     </div>
-    );
+  );
 }
 
 export default SignupForm;

3a4666ef585d3207e46ee0c3c80adb678ad4792c
Tanushree227 <tanushreedas813@gmail.com>
2024-01-15 15:20:14 +0530

    storing signup data to database

--- src/components/share/SignupForm.js
+++ src/components/share/SignupForm.js
@@ -2,34 +2,58 @@ import React from "react";
 import { Form, Input } from "antd";
 import { Link } from "react-router-dom";
 import "./signupform.css";
-function SignupForm()
+import { useRef } from 'react';
+
+function SignupForm(props)
 {
-    const onFinishHandler = (values) => {
-        console.log("Success:", values);
-      };
+  const fullnameInputRef = useRef();
+    const emailInputRef = useRef();
+    const phoneInputRef = useRef();
+    const passwordInputRef = useRef();
+    const c_passwordInputRef = useRef();
+
+    function submitHandler(event) 
+    {
+        event.preventDefault();
+
+        const enteredName = fullnameInputRef.current.value;
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPhone = phoneInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+        const enteredCPassword = c_passwordInputRef.current.value;
+
+        const userData = {
+            fullname: enteredName,
+            email: enteredEmail,
+            phone: enteredPhone,
+            password: enteredPassword,
+            c_password: enteredCPassword
+        };
+        props.onSignup(userData);
+    };
       
     return (
         <div className="form-container">
       <Form
         layout="vertical"
-        onFinish={onFinishHandler}
-        className="register-form card p-5"
+        onSubmit={submitHandler}
+        className="register-form card p-4"
       >
         <h1 className="text-center">Create your Account</h1>
         <Form.Item label="Name" name="fullname">
-          <Input type="text" placeholder="Enter your Full Name" required />
+          <Input type="text" placeholder="Enter your Full Name" ref={fullnameInputRef} required />
         </Form.Item>
         <Form.Item label="Email" name="email">
-          <Input type="email" placeholder="Enter your Email Id" required />
+          <Input type="email" placeholder="Enter your Email Id" ref={emailInputRef} required />
         </Form.Item>
         <Form.Item label="Phone" name="phone">
-          <Input type="phone" placeholder="Enter your Phone Number" required />
+          <Input type="phone" placeholder="Enter your Phone Number" ref={phoneInputRef} required />
         </Form.Item>
         <Form.Item label="Password" name="password">
-          <Input type="password" placeholder="Enter your Password" required />
+          <Input type="password" placeholder="Enter your Password" ref={passwordInputRef} required />
         </Form.Item>
         <Form.Item label="Confirm Password" name="c_password">
-          <Input type="password" placeholder="Re-Type your Password" required />
+          <Input type="password" placeholder="Re-Type your Password" ref={c_passwordInputRef} required />
         </Form.Item>
         <p>
           Already have an Account?
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,27 @@
 import React from "react";
 import SignupForm from "../components/share/SignupForm";
+import { useNavigate } from "react-router-dom";
 
 function Signup()
 {
+    const navigate = useNavigate();
+
+    function addUserHandler(userData)
+    {
+        fetch('https://reflect-f0a06-default-rtdb.firebaseio.com/user.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(userData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        ).then(() => {
+            navigate('/');
+        });
+    }
     return(
-            <SignupForm />
+            <SignupForm onSignup={addUserHandler} />
     );
 }
 
